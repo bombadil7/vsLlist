2f55b34b361f0626f08319d8cd7a40a8f418246a
bombadil7 <akniazev@gmail.com>
2017-05-07 15:25:58 -0700

    Dynamically allocate the list, print it, and then remove in main function.

--- linkedList/main.cpp
+++ linkedList/main.cpp
@@ -11,6 +11,7 @@ int main(void) {
 	char inBuffer[50];
 	cout << "Creating the list\n";
 
+	/*
 	node first, second;
 	first.num = 3;
 	first.next = &second;
@@ -20,7 +21,48 @@ int main(void) {
 	cout << "First num is " << first.num
 		<< "\nSecond num is " << (*first.next).num
 		<< "\n";
+	*/
 
-	cin.get(inBuffer, 50);
+	// Create the list -----------------------
+	node *head = NULL, *next = NULL;
+	node *current = new (node);
+	current->num = 1;
+	current->next = NULL;
+	head = current;
+
+	for (int i = 1; i < 5; ++i) {
+		// Create new node
+		node *next = new (node);
+		next->next = NULL;
+		next->num = pow(10, i);
+//		next->num = i + 1;
+
+		// Attach new node to the list
+		current->next = next;
+		current = next;
+	}
+
+	// Print the list ------------------------
+	current = head;
+	do {
+		cout << current->num << endl;
+		current = current->next;
+	} while (current != NULL);
+
+	// Delete the list -----------------------
+	current = head;
+	while (current != NULL) {
+		next = current->next;
+		free(current);
+		head = next;
+		current = next;
+	}
+
+	if (!head)
+		cout << "The entire list has been removed\n";
+	else
+		cout << "Could not remove the list\n";
+
+	cin.get(inBuffer, 5);
 	return 0;
 }
